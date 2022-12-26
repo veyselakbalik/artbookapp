0e1568c27f8795b11d523927b3450d429c950252
veyselakbalik <akbalikveyc@gmail.com>
2022-12-26 20:15:00 +0300

    image selecting method added

--- ArtBookApp/DetailsVC.swift
+++ ArtBookApp/DetailsVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameText: UITextField!
@@ -19,13 +19,31 @@ class DetailsVC: UIViewController {
 
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
-    }
-    
-
-    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
     }
     
     @objc func hideKeyboard(){
         view.endEditing(true)
     }
+    
+    @objc func selectImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
+
+    @IBAction func saveButtonClicked(_ sender: Any) {
+    }
+    
+    
 }
