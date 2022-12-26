821b845970f4d49ef9b2a62af6c6fee1c81bfe4e
veyselakbalik <akbalikveyc@gmail.com>
2022-12-26 19:38:46 +0300

    design completed

---
+++ ArtBookApp/DetailsVC.swift
@@ -0,0 +1,28 @@
+//
+//  DetailsVC.swift
+//  ArtBookApp
+//
+//  Created by Veysel Akbalık on 26.12.2022.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func saveButtonClicked(_ sender: Any) {
+    }
+    
+
+}
